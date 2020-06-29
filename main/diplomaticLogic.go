7b4ed80223211579ae083c6009d4b2c6f56cf821
secen <gabi.axinte10@gmail.com>
2020-06-29 20:51:09 +0300

    poor usage to parse

--- main/diplomaticLogic.go
+++ main/diplomaticLogic.go
@@ -2,138 +2,141 @@ package main
 
 import "strconv"
 
-func execDiplomaticActions(pickedNation Country, countryToInfluence Country){
+func execDiplomaticActions(pickedNation Country, countryToInfluence Country) {
 	var (
-		commands = map[string]func(Country, Country) (Country, Country, string) {
-			"declare war": DIPLODeclareWarOnCountry,
-			"claim": DIPLOClaimInterface,
+		commands = map[string]func(Country, Country) (Country, Country, string){
+			"declare war":    DIPLODeclareWarOnCountry,
+			"claim":          DIPLOClaimInterface,
 			"renounce claim": DIPLORenounceClaimInterface,
 			"offer alliance": DIPLOOfferAlliance,
 			"break alliance": DIPLOBreakAlliance,
-			"show values": DIPLOShowCountryValues,
-			"insult": DIPLOInsult,
-			"improve": DIPLOImproveRelations,
-			"diplomat": DIPLOViewDiplomat,
-			"spy": DIPLOViewSpy,
-		};
-		);
+			"show values":    DIPLOShowCountryValues,
+			"insult":         DIPLOInsult,
+			"improve":        DIPLOImproveRelations,
+			"diplomat":       DIPLOViewDiplomat,
+			"spy":            DIPLOViewSpy,
+		}
+	)
+	commands["declare war"](pickedNation, countryToInfluence)
 }
 
 func DIPLOClaimInterface(country4 Country, country2 Country) (country Country, country3 Country, s string) {
-	return country4,country2,"Claim Claimed"
+	return country4, country2, "Claim Claimed"
 }
 
 func DIPLORenounceClaimInterface(country4 Country, country2 Country) (country Country, country3 Country, s string) {
-	return country4,country2,"Claim Renounced"
+	return country4, country2, "Claim Renounced"
 }
 
 func DIPLOViewSpy(pickedNation Country, country2 Country) (country Country, country3 Country, s string) {
-	return pickedNation,country2,"Spy Viewed";
+	return pickedNation, country2, "Spy Viewed"
 }
 
 func DIPLOViewDiplomat(pickedNation Country, country2 Country) (country Country, country3 Country, s string) {
-	return pickedNation,country2,"Diplomat Viewed";
+	return pickedNation, country2, "Diplomat Viewed"
 }
 
-func DIPLOShowCountryValues(pickedNation Country, to Country) (country Country, country2 Country,str string) {
+func DIPLOShowCountryValues(pickedNation Country, to Country) (country Country, country2 Country, str string) {
 
-	aux:=""
-	b:=[]byte("");
-	for i,val := range(pickedNation.relations){
-		b = strconv.AppendInt(b, int64(val.Opinionvalue),10)
-		aux+=val.Cnt2.Name+":"+ string(b);
-		switch(val.Rel){
+	aux := ""
+	b := []byte("")
+	for _, val := range pickedNation.relations {
+		b = strconv.AppendInt(b, int64(val.Opinionvalue), 10)
+		aux += val.Cnt2.Name + ":" + string(b)
+		switch val.Rel {
 		case PEACE:
-			aux+=": at peace\n"
-			break;
+			aux += ": at peace\n"
+			break
 		case WAR:
-			aux+=": at war\n"
-			break;
+			aux += ": at war\n"
+			break
 		case CORDIAL:
-			aux+=": in cordial relations\n"
-			break;
+			aux += ": in cordial relations\n"
+			break
 		case THREATENED:
-			aux+=": feels threatened by us\n"
-			break;
+			aux += ": feels threatened by us\n"
+			break
 		case HISTORICAL:
-			aux+=": historical relationship\n"
-			break;
+			aux += ": historical relationship\n"
+			break
 		case ALLIED:
-			aux+=": in alliance\n"
-			break;
+			aux += ": in alliance\n"
+			break
 		}
 	}
-	return pickedNation,to,aux;
+	return pickedNation, to, aux
 }
 
-func DIPLOBreakAlliance(nation Country, to Country) (country Country, country3 Country,str string) {
-	nation.relations=append(nation.relations, RelationEntry{
+func DIPLOBreakAlliance(nation Country, to Country) (country Country, country3 Country, str string) {
+	nation.relations = append(nation.relations, RelationEntry{
 		Cnt1:         nation,
 		Cnt2:         to,
 		Rel:          THREATENED,
 		Opinionvalue: 150,
 	})
-	to.relations=append(to.relations, RelationEntry{
+	to.relations = append(to.relations, RelationEntry{
 		Cnt1:         to,
 		Cnt2:         nation,
 		Rel:          THREATENED,
 		Opinionvalue: 150,
 	})
-	return nation,to,"Alliance Broken";
-	}
+	return nation, to, "Alliance Broken"
+}
 
 func DIPLOImproveRelations(pickedCountry Country, countryToDeclareOn Country) (country Country, country3 Country, str string) {
-	pickedCountry.relations = append(pickedCountry.relations, RelationEntry{pickedCountry,countryToDeclareOn,CORDIAL,20})
-	countryToDeclareOn.relations = append(countryToDeclareOn.relations, RelationEntry{pickedCountry,countryToDeclareOn,CORDIAL,20})
-	return pickedCountry,countryToDeclareOn,"Relations Improved";
+	pickedCountry.relations = append(pickedCountry.relations, RelationEntry{pickedCountry, countryToDeclareOn, CORDIAL, 20})
+	countryToDeclareOn.relations = append(countryToDeclareOn.relations, RelationEntry{pickedCountry, countryToDeclareOn, CORDIAL, 20})
+	return pickedCountry, countryToDeclareOn, "Relations Improved"
 }
 
-func DIPLOOfferAlliance(pickedNation Country, countryToAllyTo Country) (country Country, country3 Country,str string) {
-	if(isAllianceOfInterest(pickedNation,countryToAllyTo)){
-		return DIPLOCreateAlliance(pickedNation,countryToAllyTo)
-	} else{
-		return pickedNation,countryToAllyTo,"Alliance Not Made";
+func DIPLOOfferAlliance(pickedNation Country, countryToAllyTo Country) (country Country, country3 Country, str string) {
+	if isAllianceOfInterest(pickedNation, countryToAllyTo) {
+		return DIPLOCreateAlliance(pickedNation, countryToAllyTo)
+	} else {
+		return pickedNation, countryToAllyTo, "Alliance Not Made"
 	}
 }
 
-func DIPLOCreateAlliance(nation Country, to Country) (Country,Country,string){
-	nation.relations=append(nation.relations, RelationEntry{
+func DIPLOCreateAlliance(nation Country, to Country) (Country, Country, string) {
+	nation.relations = append(nation.relations, RelationEntry{
 		Cnt1:         nation,
 		Cnt2:         to,
 		Rel:          ALLIED,
 		Opinionvalue: 150,
 	})
-	to.relations=append(to.relations, RelationEntry{
+	to.relations = append(to.relations, RelationEntry{
 		Cnt1:         to,
 		Cnt2:         nation,
 		Rel:          ALLIED,
 		Opinionvalue: 150,
 	})
-	return nation,to,"Alliance Created";
+	return nation, to, "Alliance Created"
 }
 func isAllianceOfInterest(nation Country, to Country) bool {
-	if (DIPLOgetRelations(nation,to)>100) {
-		return true;}
-	return false;
+	if DIPLOgetRelations(nation, to) > 100 {
+		return true
+	}
+	return false
 }
 
 func DIPLOgetRelations(nation Country, to Country) int32 {
-	var relationSum int32 = 0;
-	for i,rel := range(nation.relations){
-		if (rel.Cnt1.Code == nation.Code && rel.Cnt2.Code == to.Code){
-			relationSum+=rel.Opinionvalue;}
+	var relationSum int32 = 0
+	for _, rel := range nation.relations {
+		if rel.Cnt1.Code == nation.Code && rel.Cnt2.Code == to.Code {
+			relationSum += rel.Opinionvalue
+		}
 	}
-	return relationSum;
+	return relationSum
 }
 
-func DIPLOInsult(pickedCountry Country, countryToDeclareOn Country) (country Country, country3 Country,str string) {
-	pickedCountry.relations = append(pickedCountry.relations, RelationEntry{pickedCountry,countryToDeclareOn,THREATENED,-20})
-	countryToDeclareOn.relations = append(countryToDeclareOn.relations, RelationEntry{pickedCountry,countryToDeclareOn,THREATENED,-20})
-	return pickedCountry,countryToDeclareOn,"Country Insulted"
+func DIPLOInsult(pickedCountry Country, countryToDeclareOn Country) (country Country, country3 Country, str string) {
+	pickedCountry.relations = append(pickedCountry.relations, RelationEntry{pickedCountry, countryToDeclareOn, THREATENED, -20})
+	countryToDeclareOn.relations = append(countryToDeclareOn.relations, RelationEntry{pickedCountry, countryToDeclareOn, THREATENED, -20})
+	return pickedCountry, countryToDeclareOn, "Country Insulted"
 }
 
-func DIPLODeclareWarOnCountry(pickedCountry Country, countryToDeclareOn Country) (country Country, country3 Country,str string) {
+func DIPLODeclareWarOnCountry(pickedCountry Country, countryToDeclareOn Country) (country Country, country3 Country, str string) {
 	pickedNation.relations = append(pickedNation.relations, RelationEntry{pickedNation, countryToDeclareOn, WAR, -200})
 	countryToDeclareOn.relations = append(countryToDeclareOn.relations, RelationEntry{pickedNation, countryToDeclareOn, WAR, -200})
-	return pickedNation,countryToDeclareOn,"War Declared"
+	return pickedNation, countryToDeclareOn, "War Declared"
 }
--- main/init.go
+++ main/init.go
@@ -1,9 +1,10 @@
 package main
 
 var countryCodes map[int]string
-func initCountries () []Country{
+
+func initCountries() []Country {
 	var auxvec []Country
-	auxvec = make([]Country,10)
+	auxvec = make([]Country, 10)
 	var auxcnt = Country{
 		Code:                1,
 		Money:               30,
@@ -19,27 +20,28 @@ func initCountries () []Country{
 	auxvec[0] = auxcnt
 	return auxvec
 }
-func initGoods()[]Good{
+func initGoods() []Good {
 	return []Good{}
 }
-func initCountryRelations (countries []Country) []RelationEntry {
-	var relations = make([]RelationEntry,10)
-	relations[0] = RelationEntry{cnt1:countries[0],cnt2:countries[1],rel:PEACE}
+func initCountryRelations(countries []Country) []RelationEntry {
+	var relations = make([]RelationEntry, 10)
+	relations[0] = RelationEntry{Cnt1: countries[0], Cnt2: countries[1], Rel: PEACE}
 	return relations
 }
 
 //noinspection ALL
 func initGameView() Controller {
-var gameView View
+	var gameView View
 	var gameOutputView View
 	var (
-	gameController = *NewController(gameView,parseGameData,func(cntrl Controller)Controller{cntrl.output= parseGameData(gameView);return cntrl },gameOutputView))
-gameController = gameController.run(gameController)
-return gameController
+		gameController = *NewController(gameView, parseGameData, func(cntrl Controller) Controller { cntrl.output = parseGameData(gameView); return cntrl }, gameOutputView)
+	)
+	gameController = gameController.run(gameController)
+	return gameController
 }
-func initAll() ([]Country,[]RelationEntry){
+func initAll() ([]Country, []RelationEntry) {
 	print("Loading assets....")
-	var countries = make([]Country,10)
+	var countries = make([]Country, 10)
 	countries = initCountries()
 	var relations = initCountryRelations(countries)
 	return countries, relations
